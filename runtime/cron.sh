6a4d470aaa2dfdd2722232183b0d83806a9d6576
khdevnet <khdevnet@gmail.com>
2026-05-13 18:19:16 +0200

    clean up

--- runtime/cron.sh
+++ runtime/cron.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Run afk_fix_prs every 60 seconds
 while true; do
-  GH_CONFIG_DIR=/home/ubuntu/.config/gh afk_fix_prs \
+  afk_fix_prs \
     --repo_dir "$AFK_REPO_DIR" \
     --github_user "$AFK_GITHUB_USER" \
     --github_repo "$AFK_GITHUB_REPO"
