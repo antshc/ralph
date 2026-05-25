11babb1d2b9d189e73cdc545cce80b157e48b77f
khdevnet <khdevnet@gmail.com>
2026-05-25 12:55:13 +0200

    delete fix pr repo dir

--- src/cron.sh
+++ src/cron.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 while true; do
   afk_fix_prs \
-    --repo_dir "/home/ubuntu/workspace" \
     --github_user "$AFK_GITHUB_USER" \
     --github_repo "$AFK_GITHUB_REPO"
   sleep "$AFK_RALPH_FIX_PR_SLEEP"
